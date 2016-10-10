cd26a875baa3fe53dd2a975c2e57975c3a54d2f1
henrycao <631079326@qq.com>
2016-10-10 10:42:35 +0800

    重构项目

---
+++ jeedcp-base-service/src/main/java/com/jeedcp/common/service/BaseService.java
@@ -0,0 +1,27 @@
+/**
+ * Copyright &copy; 2015-2015  Metinform All rights reserved.
+ */
+package com.jeedcp.common.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+/**
+ * Service基类
+ * @author Zach Xu
+ * @version 2014-05-16
+ */
+@Transactional(readOnly = true)
+public abstract class BaseService {
+	
+	/**
+	 * 日志对象
+	 */
+	protected Logger logger = LoggerFactory.getLogger(getClass());
+
+
+
+}
