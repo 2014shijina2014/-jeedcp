53f6d04e2fbb1c3a6f9f12b4e32a032f475e8d97
henrycao <631079326@qq.com>
2016-10-10 10:42:03 +0800

    重构项目

---
+++ jeedcp-base-persistence-sql/src/main/java/com/jeedcp/common/persistence/BaseDao.java
@@ -0,0 +1,13 @@
+/**
+ * Copyright &copy; 2015-2015  Metinform All rights reserved.
+ */
+package com.jeedcp.common.persistence;
+
+/**
+ * DAO支持类实现
+ * @author Zach Xu
+ * @version 2014-05-16
+ */
+public interface BaseDao {
+
+}
---
+++ jeedcp-base-persistence-sql/src/main/java/com/jeedcp/common/persistence/DataEntity.java
@@ -0,0 +1,104 @@
+/**
+ * Copyright &copy; 2015-2015  Metinform All rights reserved.
+ */
+package com.jeedcp.common.persistence;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.hibernate.validator.constraints.Length;
+
+import java.util.Date;
+
+/**
+ * 数据Entity类
+ * @author Zach Xu
+ * @version 2014-05-16
+ */
+public abstract class DataEntity<T> extends BaseEntity<T> {
+
+	private static final long serialVersionUID = 1L;
+	
+	protected String remarks;	// 备注
+	protected String createBy;	// 创建者
+	protected Date createDate;	// 创建日期
+	protected String updateBy;	// 更新者
+	protected Date updateDate;	// 更新日期
+	protected String delFlag; 	// 删除标记（0：正常；1：删除；2：审核）
+
+    private String orderBy;//TODO: 解耦page时的临时解决方案：将entity.page.orderby移动至entity.orderBy
+
+    @JsonIgnore
+    public String getOrderBy() {
+        return orderBy;
+    }
+
+    public void setOrderBy(String orderBy) {
+        this.orderBy = orderBy;
+    }
+
+    public DataEntity() {
+		super();
+		this.delFlag = DEL_FLAG_NORMAL;
+	}
+	
+	public DataEntity(String id) {
+		super(id);
+	}
+
+
+	@Length(min=0, max=255)
+	public String getRemarks() {
+		return remarks;
+	}
+
+	public void setRemarks(String remarks) {
+		this.remarks = remarks;
+	}
+	
+	@JsonIgnore
+	public String getCreateBy() {
+		return createBy;
+	}
+
+	public void setCreateBy(String createBy) {
+		this.createBy = createBy;
+	}
+
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+	public Date getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+
+	@JsonIgnore
+	public String getUpdateBy() {
+		return updateBy;
+	}
+
+	public void setUpdateBy(String updateBy) {
+		this.updateBy = updateBy;
+	}
+
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+	public Date getUpdateDate() {
+		return updateDate;
+	}
+
+	public void setUpdateDate(Date updateDate) {
+		this.updateDate = updateDate;
+	}
+
+	@JsonIgnore
+	@Length(min=1, max=1)
+	public String getDelFlag() {
+		return delFlag;
+	}
+
+	public void setDelFlag(String delFlag) {
+		this.delFlag = delFlag;
+	}
+
+}
