9f4566aaaa644d62e90e99dbc629e516e441b7be
henrycao <631079326@qq.com>
2016-10-10 10:45:56 +0800

    重构项目

---
+++ jeedcp-base-webapp-support/src/main/java/com/jeedcp/common/exception/MetMappingExceptionResolver.java
@@ -0,0 +1,59 @@
+package com.jeedcp.common.exception;
+
+import com.jeedcp.common.mapper.JsonMapper;
+import com.jeedcp.common.web.Servlets;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.SimpleMappingExceptionResolver;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by Zach on 15/10/28.
+ */
+public class MetMappingExceptionResolver extends SimpleMappingExceptionResolver{
+
+
+
+    @Override
+    protected ModelAndView doResolveException(HttpServletRequest request,
+                                              HttpServletResponse response, Object handler, Exception ex) {
+        setDefaultStatusCode(404);
+        // Expose ModelAndView for chosen error view.
+        String viewName = determineViewName(ex, request);
+        // Apply HTTP status code for error views, if specified.
+        // Only apply it if we're processing a top-level request.
+        Integer statusCode = determineStatusCode(request, viewName);
+        if (viewName != null) {// JSP格式返回
+            if (!Servlets.isAjaxRequest(request)) {
+                // 如果不是异步请求
+                if (statusCode != null) {
+                    applyStatusCodeIfPossible(request, response, statusCode);
+                }
+                return getModelAndView(viewName, ex, request);
+            } else {// JSON格式返回
+                String json = JsonMapper.toJsonString(ex);
+                ex.setStackTrace(new StackTraceElement[]{});
+                response.setStatus(statusCode);
+                PrintWriter writer = null;
+                try {
+                     writer = response.getWriter();
+                    writer.write(json);
+                    writer.flush();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }finally {
+                    if(writer!=null)
+                        writer.close();
+                    return null;
+                }
+//                return null;
+
+            }
+        } else {
+            return null;
+        }
+    }
+}
---
+++ jeedcp-base-webapp-support/src/main/java/com/jeedcp/common/i18n/I18nMessageServiceBean.java
@@ -0,0 +1,24 @@
+package com.jeedcp.common.i18n;
+
+import com.jeedcp.common.utils.SpringContextHolder;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.servlet.LocaleResolver;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Locale;
+
+/**
+ * Created by Zach on 15/9/23.
+ */
+public class I18nMessageServiceBean {
+    /**
+     * 获得i18n字符串
+     */
+    public static String getMessage(String code, Object[] args) {
+        LocaleResolver localLocaleResolver = (LocaleResolver) SpringContextHolder.getBean(LocaleResolver.class);
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+        Locale localLocale = localLocaleResolver.resolveLocale(request);
+        return SpringContextHolder.getApplicationContext().getMessage(code, args, localLocale);
+    }
+}
